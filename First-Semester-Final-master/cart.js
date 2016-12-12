b8cac81241a7201c58a3bd1383befed9071d1f5b
MarcusMendoza <7mmendoza@stpatrick.org>
2016-12-12 13:54:56 -0600

    Add files via upload

---
+++ First-Semester-Final-master/cart.js
@@ -0,0 +1,14 @@
+class Cart{
+  //What is the first part of every class? Type it below.
+constructor (){
+this.itemList=l[];
+this.itemQuantity=[];
+}
+
+}
+
+
+  //Type the instance functions below this comment.
+
+
+}
---
+++ First-Semester-Final-master/item.js
@@ -0,0 +1,14 @@
+class Item{
+  //Fill in the item class below this comment.
+constructor (n,p,s){
+this.name=n
+this.price=p
+this.shipping=s
+
+}
+}
+
+//Create your three test items below this comment.
+new Item acf("action figure",15,3);
+new Item plc("playing cards",8,2);
+new Item bot("boop toy",10,3);
