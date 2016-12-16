d896298ce879629a5b378f1e23da5a9b0b12f475
MarcusMendoza <7mmendoza@stpatrick.org>
2016-12-16 13:57:13 -0600

    Add files via upload

--- First-Semester-Final-master/password.js
+++ First-Semester-Final-master/password.js
@@ -7,8 +7,27 @@ constructor(k,p){
 
 
   //Instance functions below this comment.
+ValidPublickey(){
+if(this.publickey.length >=8 && this.publickey.length <=25){
+return true
 
-
-
-  //Static function below this comment.
+else{
+return false;
 }
+
+}
+}
+ValidPrivatekey(){
+  if(this.privatekey.char([4]&& this.privatekey[9]=="-")){
+  return true;
+
+}
+else{
+  return false;
+}
+  //Static function below this comment.
+makePrivateKey(){
+  let key = [];
+  let limit = 14;
+  for(c = 0;c <= 3; c++){
+  }
